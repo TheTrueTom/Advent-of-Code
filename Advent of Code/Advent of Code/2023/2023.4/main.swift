82935ff88a7f786a742e9871698b96f4990bf426
TheTrueTom <thomas@brichart.fr>
2023-12-04 15:39:56 +0100

    2023 - Day 4 - Optimization

--- Advent of Code/Advent of Code/2023/2023.4/main.swift
+++ Advent of Code/Advent of Code/2023/2023.4/main.swift
@@ -11,12 +11,11 @@ func main() throws {
     let input = try readInput(fromTestFile: false).split(separator: "\n")
     
     var resDic = [Int: Int]()
-    var copiesDic = [Int: Int]()
     
     var result1 = 0
     
     for line in input {
-        var cardNumber = Int(line.split(separator: ":")[0].replacingOccurrences(of: "Card", with: "").replacingOccurrences(of: " ", with: ""))!
+        let cardNumber = Int(line.split(separator: ":")[0].replacingOccurrences(of: "Card", with: "").replacingOccurrences(of: " ", with: ""))!
         let winningNumbers = Set(line.split(separator: ":")[1].split(separator: "|")[0].split(separator: " ").compactMap { Int($0)! })
         let userNumbers = Set(line.split(separator: ":")[1].split(separator: "|")[1].split(separator: " ").compactMap { Int($0)! })
         
@@ -28,29 +27,21 @@ func main() throws {
     
     print("Part1: \(result1)")
     
-    for i in 1...resDic.keys.count {
-        copiesDic[i] = 1
-    }
+    var copies = [Int](repeating: 1, count: input.count)
     
-    for i in 1...copiesDic.keys.count {
-        
-        // If we need to add card copies
-        if resDic[i]! > 0 {
-            
-            // For each card copy
-            for _ in 1...copiesDic[i]! {
-                
-                // We add the number of following cards
-                for j in 1...resDic[i]! {
-                    if i + j <= resDic.keys.max()! {
-                        copiesDic[i + j] = copiesDic[i + j]! + 1
-                    }
-                }
+    // Loop over the cards
+    for i in 0...input.count-1 {
+        // If we have to add cards
+        if resDic[i+1]! > 0 {
+            // Add cards along the following numbers
+            for j in i+1...i+resDic[i+1]! {
+                // Add every time the number of cards needed (being the number of cards of the current considered card)
+                copies[j] += copies[i]
             }
         }
     }
     
-    let result2 = copiesDic.values.reduce(0, +)
+    let result2 = copies.reduce(0, +)
     print("Part2: \(result2)")
 }
 
