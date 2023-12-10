f70a1bd07c3b9e4c12b257b9a221b356a69de79d
TheTrueTom <thomas@brichart.fr>
2023-12-10 16:30:50 +0100

    2023 - Day 10 - Better & faster solution

--- Advent of Code/Advent of Code/2023/2023.10/main.swift
+++ Advent of Code/Advent of Code/2023/2023.10/main.swift
@@ -17,14 +17,13 @@ extension String {
     }
 }
 
-/// Pipe connections in format (N, S, E, W)
 let connections = [
-    "|": "NS", // (n: 1, s: 1, e: 0, w: 0), // vertical pipe connecting north and south.
-    "-": "EW", // (n: 0, s: 0, e: 1, w: 1), // horizontal pipe connecting east and west.
-    "L": "NE", // (n: 1, s: 0, e: 1, w: 0), // 90-degree bend connecting north and east.
-    "J": "NW", // (n: 1, s: 0, e: 0, w: 1), // 90-degree bend connecting north and west.
-    "7": "SW", // (n: 0, s: 1, e: 0, w: 1), // 90-degree bend connecting south and west.
-    "F": "SE" // (n: 0, s: 1, e: 1, w: 0), // 90-degree bend connecting south and east.
+    "|": "NS",
+    "-": "EW",
+    "L": "EN",
+    "J": "NW",
+    "7": "SW",
+    "F": "ES"
 ]
 
 let directions = [
@@ -34,57 +33,60 @@ let directions = [
     "W": (0, -1)
 ]
 
+func sReplacement(map: [[String]], sPosition: (Int, Int)) -> String {
+    var sConnections = [String]()
+    
+    if "|F7".contains(map[sPosition.0 - 1][sPosition.1]) {
+        sConnections.append("N")
+    }
+    
+    if "|JL".contains(map[sPosition.0 + 1][sPosition.1]) {
+        sConnections.append("S")
+    }
+    
+    if "-FL".contains(map[sPosition.0][sPosition.1 - 1]) {
+        sConnections.append("W")
+    }
+    
+    if "-J7".contains(map[sPosition.0][sPosition.1 + 1]) {
+        sConnections.append("E")
+    }
+    
+    let direction = sConnections.sorted().joined()
+    
+    return connections.filter { $0.value == direction }.keys.first!
+}
+
 func main() throws {
     let input = try readInput(fromTestFile: false).split(separator: "\n")
     
-    let theMap = input.map { $0.split(separator: "").map { String($0) } }
+    var theMap = input.map { $0.split(separator: "").map { String($0) } }
+    
+    // Find the starting position
     let Sposition = theMap.enumerated().filter { (i, v) in v.contains("S") }.map { ($0.offset, $0.element
         .firstIndex(of: "S")!.advanced(by: 0)) }[0]
     
-    var possibleInitialDirections = [String]()
-    
-    for elt in "NSEW" {
-        let initialDirection = String(elt)
-        
-        guard (0..<theMap.count).contains(Sposition.0 + directions[initialDirection]!.0) else { continue }
-        guard (0..<theMap[Sposition.0 + directions[initialDirection]!.0].count).contains(Sposition.1 + directions[initialDirection]!.1) else { continue }
-        
-        let connection = theMap[Sposition.0 + directions[initialDirection]!.0][Sposition.1 + directions[initialDirection]!.1]
-        
-        // Check that we can go that direction
-        guard connection != "." else { continue }
-        if initialDirection == "N" && !"|7F".contains(connection) { continue }
-        if initialDirection == "S" && !"|LJ".contains(connection) { continue }
-        if initialDirection == "E" && !"-J7".contains(connection) { continue }
-        if initialDirection == "W" && !"-LF".contains(connection) { continue }
-        
-        possibleInitialDirections.append(initialDirection)
-    }
-    
-    guard possibleInitialDirections.count == 2 else { print("There is a problem"); return }
+    // Replace the start position with the symbol it should be
+    theMap[Sposition.0][Sposition.1] = sReplacement(map: theMap, sPosition: Sposition)
     
     var steps = 0
     var position = Sposition
-    var direction = String(possibleInitialDirections[0])
+    var direction = String(connections[theMap[Sposition.0][Sposition.1]]!.first!).inverse()
     
-    var allPositions = [position]
+    // Map to indicate places where we are on the path for part 2
+    var otherMap = [[String]](repeating: [String](repeating: "", count: theMap[0].count), count: theMap.count)
+    otherMap[position.0][position.1] = "X"
     
-    while theMap[position.0][position.1] != "S" || steps == 0 {
+    while position != Sposition || steps == 0 {
         var symbol = theMap[position.0][position.1]
         
-        if symbol == "S" {
-            symbol = "NS".contains(direction) ? "|" : "-"
-        }
-        
-        let connectionDirections = connections[symbol]!
-        direction = connectionDirections.replacingOccurrences(of: direction.inverse(), with: "")
+        direction = connections[symbol]!.replacingOccurrences(of: direction.inverse(), with: "")
         position = (position.0 + directions[direction]!.0, position.1 + directions[direction]!.1)
         
-        allPositions.append(position)
-        
+        otherMap[position.0][position.1] = "X"
         steps += 1
     }
-
+    
     let result1 = steps / 2
     
     print("Part1: \(result1)")
@@ -95,9 +97,9 @@ func main() throws {
     for i in 0..<theMap.count {
         for j in 0..<theMap[0].count {
             // If it is a puzzle piece
-            if let _ = allPositions.first(where: { $0 == (i, j) }) {
+            if !otherMap[i][j].isEmpty {
                 // Vertical piece
-                if "|F7S".contains(theMap[i][j]) {
+                if "|F7".contains(theMap[i][j]) {
                     inside.toggle()
                 }
             } else {
